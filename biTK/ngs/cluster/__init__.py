b0985ba74490f3a18b79c3fe1e383c3deaaddab1
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-12-02 11:05:42 +0900

    Add arab.txt data cluster

---
+++ biTK/ngs/cluster/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: davecao
+# @Date:   2015-11-28 14:23:42
+# @Last Modified by:   davecao
+# @Last Modified time: 2015-11-28 14:28:42
+from __future__ import division, print_function, absolute_import
+
+__all__ = ['hierarchy']
+
+from . import hierarchy
